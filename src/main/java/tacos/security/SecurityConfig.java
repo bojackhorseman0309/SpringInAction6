f1534f18d6df3e0938d839b9fa2820695fdd73b1
bojackHorseman0309 <aloaraya@outlook.com>
2022-03-03 19:39:54 -0600

    Chapter 8. Oauth2

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -1,271 +1,74 @@
 package tacos.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web
-                        .configuration.WebSecurityConfigurerAdapter;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation
-             .authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web
-             .builders.HttpSecurity;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@SuppressWarnings("deprecation")
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-  @Autowired
-  private UserDetailsService userDetailsService;
+    @Autowired
+    private UserDetailsService userDetailsService;
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http
-      .authorizeRequests()
-        .antMatchers("/design", "/orders").access("hasRole('USER')")
-        .antMatchers("/", "/**").access("permitAll")
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS).permitAll() // needed for Angular/CORS
+                .antMatchers(HttpMethod.POST, "/api/ingredients")
+                .hasAuthority("SCOPE_writeIngredients")
+                .antMatchers(HttpMethod.DELETE, "/api/ingredients")
+                .hasAuthority("SCOPE_deleteIngredients")
+                .antMatchers("/api/tacos", "/api/orders/**")
+                .permitAll()
+                .antMatchers("/**").access("permitAll")
+                .and()
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt())
 
-      .and()
-        .formLogin()
-          .loginPage("/login")
+                .httpBasic()
+                .realmName("Taco Cloud")
 
-      .and()
-        .logout()
-          .logoutSuccessUrl("/")
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
 
-      // Make H2-Console non-secured; for debug purposes
-      .and()
-        .csrf()
-          .ignoringAntMatchers("/h2-console/**")
+                .and()
+                .csrf()
+                .ignoringAntMatchers("/h2-console/**", "/api/**")
 
-      // Allow pages to be loaded in frames from the same origin; needed for H2-Console
-      .and()
-        .headers()
-          .frameOptions()
-            .sameOrigin()
-      ;
-  }
+                // Allow pages to be loaded in frames from the same origin; needed for H2-Console
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+        ;
+    }
 
-  /*
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth)
-      throws Exception {
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-    auth
-      .userDetailsService(userDetailsService);
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
 
-  }
-   */
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(encoder());
 
-  @Bean
-  public PasswordEncoder encoder() {
-    return new BCryptPasswordEncoder();
-  }
-
-
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth)
-      throws Exception {
-
-    auth
-      .userDetailsService(userDetailsService)
-      .passwordEncoder(encoder());
-
-  }
-
-//
-// IN MEMORY AUTHENTICATION EXAMPLE
-//
-/*
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth)
-      throws Exception {
-
-    auth
-      .inMemoryAuthentication()
-        .withUser("buzz")
-          .password("infinity")
-          .authorities("ROLE_USER")
-        .and()
-        .withUser("woody")
-          .password("bullseye")
-          .authorities("ROLE_USER");
-
-  }
-*/
-
-//
-// JDBC Authentication example
-//
-/*
-  @Autowired
-  DataSource dataSource;
-
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth)
-      throws Exception {
-
-    auth
-      .jdbcAuthentication()
-        .dataSource(dataSource);
-
-  }
-*/
-
-/*
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth)
-      throws Exception {
-
-    auth
-      .jdbcAuthentication()
-        .dataSource(dataSource)
-        .usersByUsernameQuery(
-            "select username, password, enabled from Users " +
-            "where username=?")
-        .authoritiesByUsernameQuery(
-            "select username, authority from UserAuthorities " +
-            "where username=?");
-
-  }
-*/
-
-/*
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth)
-      throws Exception {
-
-    auth
-      .jdbcAuthentication()
-        .dataSource(dataSource)
-        .usersByUsernameQuery(
-            "select username, password, enabled from Users " +
-            "where username=?")
-        .authoritiesByUsernameQuery(
-            "select username, authority from UserAuthorities " +
-            "where username=?")
-        .passwordEncoder(new BCryptPasswordEncoder());
-
-  }
-*/
-
-
-//
-// LDAP Authentication example
-//
-/*
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth)
-      throws Exception {
-    auth
-      .ldapAuthentication()
-        .userSearchFilter("(uid={0})")
-        .groupSearchFilter("member={0}");
-  }
-*/
-
-/*
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth)
-      throws Exception {
-    auth
-      .ldapAuthentication()
-        .userSearchBase("ou=people")
-        .userSearchFilter("(uid={0})")
-        .groupSearchBase("ou=groups")
-        .groupSearchFilter("member={0}");
-  }
-*/
-
-/*
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth)
-      throws Exception {
-    auth
-      .ldapAuthentication()
-        .userSearchBase("ou=people")
-        .userSearchFilter("(uid={0})")
-        .groupSearchBase("ou=groups")
-        .groupSearchFilter("member={0}")
-        .passwordCompare();
-  }
-*/
-
-/*
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth)
-      throws Exception {
-    auth
-      .ldapAuthentication()
-        .userSearchBase("ou=people")
-        .userSearchFilter("(uid={0})")
-        .groupSearchBase("ou=groups")
-        .groupSearchFilter("member={0}")
-        .passwordCompare()
-        .passwordEncoder(new BCryptPasswordEncoder())
-        .passwordAttribute("passcode");
-  }
-*/
-
-/*
-@Override
-protected void configure(AuthenticationManagerBuilder auth)
-    throws Exception {
-  auth
-    .ldapAuthentication()
-      .userSearchBase("ou=people")
-      .userSearchFilter("(uid={0})")
-      .groupSearchBase("ou=groups")
-      .groupSearchFilter("member={0}")
-      .passwordCompare()
-      .passwordEncoder(new BCryptPasswordEncoder())
-      .passwordAttribute("passcode")
-      .and()
-      .contextSource()
-        .url("ldap://tacocloud.com:389/dc=tacocloud,dc=com");
-}
-*/
-
-/*
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth)
-      throws Exception {
-    auth
-      .ldapAuthentication()
-        .userSearchBase("ou=people")
-        .userSearchFilter("(uid={0})")
-        .groupSearchBase("ou=groups")
-        .groupSearchFilter("member={0}")
-        .passwordCompare()
-        .passwordEncoder(new BCryptPasswordEncoder())
-        .passwordAttribute("passcode")
-        .and()
-        .contextSource()
-          .root("dc=tacocloud,dc=com");
-  }
-*/
-
-/*
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth)
-      throws Exception {
-    auth
-      .ldapAuthentication()
-        .userSearchBase("ou=people")
-        .userSearchFilter("(uid={0})")
-        .groupSearchBase("ou=groups")
-        .groupSearchFilter("member={0}")
-        .passwordCompare()
-        .passwordEncoder(new BCryptPasswordEncoder())
-        .passwordAttribute("passcode")
-        .and()
-        .contextSource()
-          .root("dc=tacocloud,dc=com")
-          .ldif("classpath:users.ldif");
-  }
-*/
+    }
 
 }
