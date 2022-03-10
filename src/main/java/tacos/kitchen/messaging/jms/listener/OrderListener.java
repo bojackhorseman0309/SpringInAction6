aec65682072649c946c57543fc567c7558bad9d6
bojackHorseman0309 <aloaraya@outlook.com>
2022-03-09 19:04:05 -0600

    Chapter9. Kafka

--- src/main/java/tacos/kitchen/messaging/jms/listener/OrderListener.java
+++ src/main/java/tacos/kitchen/messaging/jms/listener/OrderListener.java
@@ -1,12 +1,10 @@
 package tacos.kitchen.messaging.jms.listener;
 
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import tacos.TacoOrder;
 
-@Profile("jms-listener")
 @Component
 public class OrderListener {
     private KitchenUI ui;
@@ -16,8 +14,8 @@ public class OrderListener {
         this.ui = ui;
     }
 
-    @RabbitListener(queues = "tacocloud.order.queue")
-    public void receiveOrder(TacoOrder order) {
+    @KafkaListener(topics = "tacocloud.orders.topic")
+    public void handle(TacoOrder order) {
         ui.displayOrder(order);
     }
 }
