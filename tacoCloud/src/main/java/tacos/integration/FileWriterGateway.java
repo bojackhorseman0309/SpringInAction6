cd9a0af0b879b7652558b80b582f9235ea0a2507
bojackHorseman0309 <aloaraya@outlook.com>
2022-03-09 20:23:22 -0600

    Chapter 10. Spring Integration

---
+++ tacoCloud/src/main/java/tacos/integration/FileWriterGateway.java
@@ -0,0 +1,10 @@
+package tacos.integration;
+
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.file.FileHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+
+@MessagingGateway(defaultRequestChannel = "textInChannel")
+public interface FileWriterGateway {
+    void writeToFile(@Header(FileHeaders.FILENAME) String fileName, String data);
+}
--- tacoCloud/src/main/java/tacos/messaging/OrderMessagingService.java
+++
@@ -1,9 +0,0 @@
-package tacos.messaging;
-
-import tacos.TacoOrder;
-
-public interface OrderMessagingService {
-
-    void sendOrder(TacoOrder order);
-
-}
